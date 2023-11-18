e35e02798721e5829efb66e9feee1f991e5061ef
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-11-19 00:04:43 +0900

    Add new extensions

--- app/src/main/java/wing/tree/multiplication/table/extension/Int.kt
+++ app/src/main/java/wing/tree/multiplication/table/extension/Int.kt
@@ -5,6 +5,7 @@ package wing.tree.multiplication.table.extension
 private const val FIVE = 5
 private const val FOUR = 4
 private const val ONE = 1
+private const val SIX = 6
 private const val THREE = 3
 private const val TWO = 2
 private const val ZERO = 0
@@ -17,6 +18,7 @@ val Int.Companion.none: Int get() = ZERO
 val Int.Companion.one: Int get() = ONE
 val Int.Companion.three: Int get() = THREE
 val Int.Companion.secondIndex: Int get() = ONE
+val Int.Companion.seventhIndex: Int get() = SIX
 val Int.Companion.sixthIndex: Int get() = FIVE
 val Int.Companion.thirdIndex: Int get() = TWO
 val Int.Companion.two: Int get() = TWO
--- app/src/main/java/wing/tree/multiplication/table/extension/List.kt
+++ app/src/main/java/wing/tree/multiplication/table/extension/List.kt
@@ -3,5 +3,6 @@ package wing.tree.multiplication.table.extension
 fun <T> List<T>.fifth() = get(Int.fifthIndex)
 fun <T> List<T>.fourth() = get(Int.fourthIndex)
 fun <T> List<T>.second() = get(Int.secondIndex)
+fun <T> List<T>.seventh() = get(Int.seventhIndex)
 fun <T> List<T>.sixth() = get(Int.sixthIndex)
 fun <T> List<T>.third() = get(Int.thirdIndex)
