af0a06d65970f1280a2becd6cfa29280102d3241
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-11-19 09:28:31 +0900

    Define alpha objects

---
+++ app/src/main/java/wing/tree/multiplication/table/constant/HighContrastContentAlpha.kt
@@ -0,0 +1,9 @@
+@file:Suppress("unused")
+
+package wing.tree.multiplication.table.constant
+
+object HighContrastContentAlpha {
+    const val high: Float = 1.00F
+    const val medium: Float = 0.74F
+    const val disabled: Float = 0.38F
+}
---
+++ app/src/main/java/wing/tree/multiplication/table/constant/LowContrastContentAlpha.kt
@@ -0,0 +1,9 @@
+@file:Suppress("unused")
+
+package wing.tree.multiplication.table.constant
+
+object LowContrastContentAlpha {
+    const val high: Float = 0.87F
+    const val medium: Float = 0.60F
+    const val disabled: Float = 0.38F
+}
