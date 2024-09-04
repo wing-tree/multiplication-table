42b0ddd04fa1f67a5fd6509c455303dfccac3c27
noah <noah@supergene.co>
2024-09-04 17:24:26 +0900

    Add extensions.

---
+++ app/src/main/java/wing/tree/multiplication/table/extension/property/String.kt
@@ -0,0 +1,23 @@
+@file:Suppress("unused")
+
+package wing.tree.multiplication.table.extension.property
+
+val String.intOrNull: Int? get() = toIntOrNull()
+
+val String.Companion.colon: String get() = ":"
+val String.Companion.divisionSign: String get() = "÷"
+val String.Companion.dot: String get() = "."
+val String.Companion.empty: String get() = ""
+val String.Companion.equalsSign: String get() = "="
+val String.Companion.hyphen: String get() = "-"
+val String.Companion.leftCurlyBrace: String get() = "{"
+val String.Companion.new: String get() = "new"
+val String.Companion.numberSign: String get() = "#"
+val String.Companion.minusSign: String get() = "−"
+val String.Companion.multiplicationSign: String get() = "×"
+val String.Companion.plusSign: String get() = "+"
+val String.Companion.questionMark: String get() = "?"
+val String.Companion.rightCurlyBrace: String get() = "}"
+val String.Companion.slash: String get() = "/"
+val String.Companion.space: String get() = " "
+val String.Companion.underScore: String get() = "_"
