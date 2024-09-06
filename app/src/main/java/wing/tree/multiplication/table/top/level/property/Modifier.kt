a4235ba1134cfc24fcfbb5c3e7458afb5e44aa1a
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-06 23:40:39 +0900

    Add extension

---
+++ app/src/main/java/wing/tree/multiplication/table/top/level/property/Modifier.kt
@@ -0,0 +1,10 @@
+@file:Suppress("unused")
+
+package wing.tree.multiplication.table.top.level.property
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+
+val fillMaxSize = Modifier.fillMaxSize()
+val fillMaxWidth = Modifier.fillMaxWidth()
