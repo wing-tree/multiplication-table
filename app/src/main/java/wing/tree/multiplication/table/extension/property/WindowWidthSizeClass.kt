bcb6293ea30c1a8c1677d8c3b20953867fa82c60
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-07 03:11:47 +0900

    Apply padding values.

--- app/src/main/java/wing/tree/multiplication/table/extension/property/WindowWidthSizeClass.kt
+++ app/src/main/java/wing/tree/multiplication/table/extension/property/WindowWidthSizeClass.kt
@@ -1,18 +1,19 @@
 package wing.tree.multiplication.table.extension.property
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.ui.unit.Dp
 import wing.tree.multiplication.table.extension.extraLarge
 import wing.tree.multiplication.table.extension.function.`is`
 import wing.tree.multiplication.table.extension.function.not
 import wing.tree.multiplication.table.extension.medium
-import wing.tree.multiplication.table.model.MarginValues
 
 val WindowWidthSizeClass.isCompact: Boolean get() = `is`(WindowWidthSizeClass.Compact)
 val WindowWidthSizeClass.isNotCompact: Boolean get() = not(WindowWidthSizeClass.Compact)
-val WindowWidthSizeClass.marginValues: MarginValues get() = when (this) {
-    WindowWidthSizeClass.Compact -> MarginValues(horizontal = Dp.medium)
-    WindowWidthSizeClass.Medium -> MarginValues(horizontal = Dp.extraLarge)
-    WindowWidthSizeClass.Expanded -> MarginValues(horizontal = Dp.extraLarge)
-    else -> MarginValues(horizontal = Dp.medium)
-}
+val WindowWidthSizeClass.paddingValues: PaddingValues
+    get() = when (this) {
+        WindowWidthSizeClass.Compact -> PaddingValues(horizontal = Dp.medium)
+        WindowWidthSizeClass.Medium -> PaddingValues(horizontal = Dp.extraLarge)
+        WindowWidthSizeClass.Expanded -> PaddingValues(horizontal = Dp.extraLarge)
+        else -> PaddingValues(horizontal = Dp.medium)
+    }
