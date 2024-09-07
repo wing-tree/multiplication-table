c5d0dd633f10fc9736930f7b9d58f33394a2a92b
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-07 20:45:22 +0900

    Add token padding class.

---
+++ app/src/main/java/wing/tree/multiplication/table/token/Padding.kt
@@ -0,0 +1,56 @@
+package wing.tree.multiplication.table.token
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.unit.Dp
+import wing.tree.multiplication.table.extension.`8`
+import wing.tree.multiplication.table.extension.half
+import wing.tree.multiplication.table.extension.property.`1`
+import wing.tree.multiplication.table.extension.property.inc
+import wing.tree.multiplication.table.extension.property.pair
+import wing.tree.multiplication.table.extension.property.single
+import wing.tree.multiplication.table.extension.property.triple
+
+internal abstract class Padding(open val dp: Dp) {
+    abstract val extra: Extra
+
+    open val small: Dp
+        @Composable
+        @ReadOnlyComposable
+        get() = dp.times(Int.single)
+
+    open val medium: Dp
+        @Composable
+        @ReadOnlyComposable
+        get() = dp.times(Int.pair)
+
+    open val large: Dp
+        @Composable
+        @ReadOnlyComposable
+        get() = dp.times(Int.triple)
+
+    data class Extra internal constructor(
+        private val level: Int
+    ): Padding(dp) {
+        override val extra: Extra
+            get() = Extra(
+            level = level.inc
+        )
+
+        override val small: Dp
+            @Composable
+            get() = super.small.minus(dp.times(level).half)
+
+        override val medium: Dp
+            @Composable
+            get() = throw UnsupportedOperationException("$this")
+
+        override val large: Dp
+            @Composable
+            get() = super.large.plus(dp.times(level).half)
+    }
+
+    companion object : Padding(dp = Dp.`8`) {
+        override val extra = Extra(level = Int.`1`)
+    }
+}
