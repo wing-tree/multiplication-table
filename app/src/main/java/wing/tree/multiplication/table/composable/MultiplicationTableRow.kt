900bd401b4f96b03afea5a33c438370a53bd4fd9
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-07 16:12:15 +0900

    Cleanup

--- app/src/main/java/wing/tree/multiplication/table/composable/MultiplicationTableRow.kt
+++ app/src/main/java/wing/tree/multiplication/table/composable/MultiplicationTableRow.kt
@@ -2,12 +2,12 @@ package wing.tree.multiplication.table.composable
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import wing.tree.multiplication.table.extension.extraSmall
 import wing.tree.multiplication.table.extension.property.`1`
+import wing.tree.multiplication.table.top.level.property.fillMaxHeight
 
 @Composable
 internal fun MultiplicationTableRow(
@@ -20,18 +20,20 @@ internal fun MultiplicationTableRow(
     ) {
         MultiplicationTable(
             timesTable = timesTable,
-            modifier = Modifier
-                .fillMaxHeight()
-                .weight(weight = Float.`1`, fill = false)
+            modifier = fillMaxHeight.weight(
+                weight = Float.`1`,
+                fill = false
+            )
         )
 
         HorizontalSpacer(width = Dp.extraSmall)
 
         MultiplicationTable(
             timesTable = timesTable.inc(),
-            modifier = Modifier
-                .fillMaxHeight()
-                .weight(weight = Float.`1`, fill = false)
+            modifier = fillMaxHeight.weight(
+                weight = Float.`1`,
+                fill = false
+            )
         )
     }
 }
