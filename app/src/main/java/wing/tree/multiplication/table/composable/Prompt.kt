74fd5a56393d7f62eb13284492568e6ccdd33c26
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-07 16:33:33 +0900

    Cleanup

---
+++ app/src/main/java/wing/tree/multiplication/table/composable/Prompt.kt
@@ -0,0 +1,52 @@
+package wing.tree.multiplication.table.composable
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import wing.tree.multiplication.table.extension.function.width
+import wing.tree.multiplication.table.extension.function.wrapInSpaces
+import wing.tree.multiplication.table.extension.property.equalsSign
+import wing.tree.multiplication.table.extension.property.multiplicationSign
+import wing.tree.multiplication.table.extension.property.widestDigit
+
+@Composable
+internal fun Prompt(
+    timesTable: Int,
+    multiplicand: Int,
+    modifier: Modifier = Modifier,
+    style: TextStyle = LocalTextStyle.current
+) {
+    Row(modifier = modifier) {
+        val widestDigitWidth = "${Char.widestDigit}".width(style)
+
+        Text(
+            text = "$timesTable",
+            modifier = Modifier.width(widestDigitWidth.times("$timesTable".length)),
+            textAlign = TextAlign.Center,
+            style = style
+        )
+
+        Text(
+            text = String.multiplicationSign.wrapInSpaces(),
+            modifier = Modifier,
+            style = style
+        )
+
+        Text(
+            text = "$multiplicand",
+            modifier = Modifier.width(widestDigitWidth),
+            textAlign = TextAlign.Center,
+            style = style
+        )
+
+        Text(
+            text = String.equalsSign.wrapInSpaces(),
+            style = style
+        )
+    }
+}
--- app/src/main/java/wing/tree/multiplication/table/constant/String.kt
+++
@@ -1,4 +0,0 @@
-package wing.tree.multiplication.table.constant
-
-const val EQUALS_SIGN = "="
-const val MULTIPLICATION_SIGN = "×"
