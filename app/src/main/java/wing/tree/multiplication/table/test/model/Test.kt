e07417084dc694e47ddd94aa9a6d136cf5299390
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-07 21:53:32 +0900

    Apply changes.

--- app/src/main/java/wing/tree/multiplication/table/test/model/Test.kt
+++ app/src/main/java/wing/tree/multiplication/table/test/model/Test.kt
@@ -2,14 +2,15 @@ package wing.tree.multiplication.table.test.model
 
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
-import wing.tree.multiplication.table.constant.MAXIMUM_MULTIPLICAND
-import wing.tree.multiplication.table.constant.MINIMUM_MULTIPLICAND
-import wing.tree.multiplication.table.constant.NUMBER_OF_QUESTIONS
 import wing.tree.multiplication.table.model.Question
+import wing.tree.multiplication.table.top.level.property.MAXIMUM_MULTIPLICAND
+import wing.tree.multiplication.table.top.level.property.MINIMUM_MULTIPLICAND
+import wing.tree.multiplication.table.top.level.property.NUMBER_OF_QUESTIONS
 import kotlin.random.Random
 
-class Test private constructor(vararg elements: Question) : PersistentList<Question> by persistentListOf(elements = elements) {
-    constructor(start: Int, endInclusive: Int): this(
+class Test private constructor(vararg elements: Question) :
+    PersistentList<Question> by persistentListOf(elements = elements) {
+    constructor(start: Int, endInclusive: Int) : this(
         elements = buildList {
             for (timesTable in start..endInclusive) {
                 for (multiplicand in MINIMUM_MULTIPLICAND..MAXIMUM_MULTIPLICAND) {
