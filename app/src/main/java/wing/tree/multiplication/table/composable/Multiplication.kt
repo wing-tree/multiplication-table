33ca76e56f3a12f1164c060a4c52cf3f31632bae
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-07 22:55:44 +0900

    Extract functions.

--- app/src/main/java/wing/tree/multiplication/table/composable/Multiplication.kt
+++ app/src/main/java/wing/tree/multiplication/table/composable/Multiplication.kt
@@ -1,30 +1,38 @@
 package wing.tree.multiplication.table.composable
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
-import wing.tree.multiplication.table.extension.function.wrapInSpaces
-import wing.tree.multiplication.table.extension.property.equalsSign
+import wing.tree.multiplication.table.extension.function.width
+import wing.tree.multiplication.table.extension.property.widestDigit
+import wing.tree.multiplication.table.top.level.property.MAXIMUM_MULTIPLICAND
 
 @Composable
-internal fun Prompt(
+internal fun Multiplication(
     timesTable: Int,
     multiplicand: Int,
     modifier: Modifier = Modifier,
     style: TextStyle = LocalTextStyle.current
 ) {
+    val widestDigitWidth = "${Char.widestDigit}".width(style)
+    val product = timesTable.times(multiplicand)
+
     Row(modifier = modifier) {
-        Term(
+        Prompt(
             timesTable = timesTable,
             multiplicand = multiplicand,
             style = style
         )
 
         Text(
-            text = String.equalsSign.wrapInSpaces(),
+            text = "$product",
+            modifier = Modifier.width(
+                widestDigitWidth.times("${timesTable.times(MAXIMUM_MULTIPLICAND)}".length)
+            ),
             style = style
         )
     }
