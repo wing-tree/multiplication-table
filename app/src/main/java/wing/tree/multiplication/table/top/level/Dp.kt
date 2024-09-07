6dbecf332ae743ea074b6bfd562a409a6db3394f
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-07 22:27:52 +0900

    Cleanup.

--- app/src/main/java/wing/tree/multiplication/table/top/level/Dp.kt
+++ app/src/main/java/wing/tree/multiplication/table/top/level/Dp.kt
@@ -2,75 +2,44 @@ package wing.tree.multiplication.table.top.level
 
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
-import wing.tree.multiplication.table.constant.EQUALS_SIGN
-import wing.tree.multiplication.table.constant.MAXIMUM_MULTIPLICAND
-import wing.tree.multiplication.table.constant.MULTIPLICATION_SIGN
-import wing.tree.multiplication.table.extension.property.`4`
-import wing.tree.multiplication.table.extension.property.`6`
-import wing.tree.multiplication.table.extension.property.digit
-import wing.tree.multiplication.table.extension.property.space
-import wing.tree.multiplication.table.extension.maxWidth
-import wing.tree.multiplication.table.extension.rememberWidth
-import wing.tree.multiplication.table.extension.width
+import androidx.compose.ui.unit.times
+import wing.tree.multiplication.table.extension.function.repeat
+import wing.tree.multiplication.table.extension.function.width
+import wing.tree.multiplication.table.extension.function.wrapInSpaces
+import wing.tree.multiplication.table.extension.property.equalsSign
+import wing.tree.multiplication.table.extension.property.multiplicationSign
+import wing.tree.multiplication.table.extension.property.widestDigit
+import wing.tree.multiplication.table.extension.property.widestPrompt
+import wing.tree.multiplication.table.top.level.property.MAXIMUM_DIGITS
+import wing.tree.multiplication.table.top.level.property.MAXIMUM_MULTIPLICAND
 
 @Composable
-fun multiplicationMaxWidth(style: TextStyle = LocalTextStyle.current): Dp {
-    val maxWidth = Char.digit.maxWidth(style = style)
+internal fun multiplicationWidthFor(
+    timesTable: Int,
+    style: TextStyle = LocalTextStyle.current
+): Dp {
+    val widestDigitWidth = "${Char.widestDigit}".width(style)
+    val product = timesTable.times(MAXIMUM_MULTIPLICAND)
     val width = buildString {
-        append(EQUALS_SIGN)
-        append(MULTIPLICATION_SIGN)
-        append(String.space.repeat(Int.`4`))
+        append(Char.widestDigit.repeat("$timesTable".length))
+        append(String.equalsSign.wrapInSpaces())
+        append(Char.widestDigit)
+        append(String.multiplicationSign.wrapInSpaces())
     }
-        .width(style)
-        .plus(maxWidth.times(Int.`6`))
+        .width(style = style)
+        .plus(other = "$product".length.times(widestDigitWidth))
 
     return width
 }
 
 @Composable
-fun rememberMultiplicationMaxWidth(style: TextStyle = LocalTextStyle.current): Dp {
-    val maxWidth = Char.digit.maxWidth(style = style)
-    val width = buildString {
-        append(EQUALS_SIGN)
-        append(MULTIPLICATION_SIGN)
-        append(String.space.repeat(Int.`4`))
-    }
-        .rememberWidth(style)
-        .plus(maxWidth.times(Int.`6`))
+internal fun widestMultiplicationWidth(style: TextStyle = LocalTextStyle.current): Dp {
+    val maxWidth = "${Char.widestDigit}".width(style)
+    val width = String.widestPrompt
+        .width(style)
+        .plus(maxWidth.times(MAXIMUM_DIGITS))
 
-    return remember(width) {
-        width
-    }
-}
-
-@Composable
-fun rememberMultiplicationWidth(
-    timesTable: Int,
-    style: TextStyle = LocalTextStyle.current
-): Dp {
-    val maxWidth = Char.digit.maxWidth(style = style)
-    val other = with("$timesTable") {
-        val product = timesTable.times(MAXIMUM_MULTIPLICAND)
-
-        maxWidth.times(
-            other = length
-                .plus(other = "$product".length)
-                .plus(other = "$MAXIMUM_MULTIPLICAND".length)
-        )
-    }
-
-    val width = buildString {
-        append(EQUALS_SIGN)
-        append(MULTIPLICATION_SIGN)
-        append(String.space.repeat(n = Int.`4`))
-    }
-        .rememberWidth(style = style)
-        .plus(other = other)
-
-    return remember(width) {
-        width
-    }
+    return width
 }
