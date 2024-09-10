876f0b91ddccc45e40b1785cb866d9afc7f3600d
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-11 02:09:42 +0900

    Releasing 1.1.0 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "wing.tree.multiplication.table"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
