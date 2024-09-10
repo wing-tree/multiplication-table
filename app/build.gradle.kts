098870f93bf4f2439a0407ef0230f1e5f50361ec
SEONGJU KIM <wind.orca.pe@gmail.com>
2024-09-11 02:31:31 +0900

    Update admob ids.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "wing.tree.multiplication.table"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
